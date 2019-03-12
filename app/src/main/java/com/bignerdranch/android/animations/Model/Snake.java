b5d6f3371a9c2a8cd2c5d0585b73e4c42f2fdfce
hectorbarrios <hsteve1992@yahoo.com>
2019-03-11 22:21:14 -0500

    Started to add code on the Snake class and check the playview to move some stuff around to make thecode look elegant.

--- app/src/main/java/com/bignerdranch/android/animations/Model/Snake.java
+++ app/src/main/java/com/bignerdranch/android/animations/Model/Snake.java
@@ -1,4 +1,46 @@
 package com.bignerdranch.android.animations.Model;
 
+import com.bignerdranch.android.animations.Shapes.Pencil;
+
+import java.util.ArrayList;
+
 public class Snake {
+
+    private int mSnakeLength;
+
+    private ArrayList<Pencil> mSnakeodyCoordinates;
+
+    public Snake()
+    {
+        mSnakeodyCoordinates= new ArrayList<>();
+        mSnakeLength = 5;
+    }
+
+
+    public void initialPosition(int width, int height)
+    {
+        mSnakeodyCoordinates.add(new Pencil(width/2, height/2));
+    }
+
+    public void addSnakeCoordinate(int x, int y)
+    {
+        mSnakeodyCoordinates.add(new Pencil(x, y));
+    }
+
+    public void checkSnakeLength()
+    {
+        if(mSnakeodyCoordinates.size() > mSnakeLength)
+        mSnakeodyCoordinates.remove(0);
+    }
+
+    public void  addSnakeLength(int value)
+    {
+        mSnakeLength += value;
+    }
+
+    public ArrayList<Pencil> getSnakebodycoordinates() {
+        return mSnakeodyCoordinates;
+    }
+
+
 }
